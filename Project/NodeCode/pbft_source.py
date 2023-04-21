efbbc1e6a1be6517fa0f602293d0ab35e0d86bca
shivang11 <guptadeepshi9@gmail.com>
2023-04-21 12:15:28 +0530

    heap

--- Project/NodeCode/pbft_source.py
+++ Project/NodeCode/pbft_source.py
@@ -1,3 +1,4 @@
+import heapq
 class pBFT:
 	def __init__(self,identity:str,nodeList:list,networkObject):
 		self.identity = identity
@@ -10,6 +11,10 @@ class pBFT:
 		self.f = len(self.nodeList)//3
 		self.n = len(self.nodeList)
 		self.cPhase = 'propose'
+		self.delay = 100
+		self.heap = []
+		for node in nodeList:
+			self.heap.heappush(self.heap , (0,node))
 	def boradCast(self,message:dict):
 		for node in nodeList:
 			self.networkingObject.sendMessage(node,message)
@@ -21,12 +26,12 @@ class pBFT:
 			return False
 		else:
 			self.votingPhase['recvIds'].append(message['sender'])
-		if message['value'] not in votingPhase['rmsg'].keys():
-			votingPhase['rmsg'][message['value']] = []
-		votingPhase['rmsg'][message['value']].append(message['sender'])
-		if len(votingPhase['rmsg'][message['value']]) >= n - f:
+		if message['value'] not in self.votingPhase['rmsg'].keys():
+			self.votingPhase['rmsg'][message['value']] = []
+		self.votingPhase['rmsg'][message['value']].append(message['sender'])
+		if len(self.votingPhase['rmsg'][message['value']]) >= n - f:
 			self.cPhase = 'precommit'
-			votingPhase = {'recvIds':[],'rmsg':{}}
+			self.votingPhase = {'recvIds':[],'rmsg':{}}
 			return True 
 		else:
 			# duplicate message ignore this bitch 
@@ -47,22 +52,24 @@ class pBFT:
 			return False
 		else:
 			self.preCommitPhase['recvIds'].append(message['sender'])
-		if message['digest'] not in self.preCommitPhase.keys():
-			# new value 
-			pass
-		elif message['identity'] not in preCommitPhase[message['digest']].keys():
-			# new entry 
-			pass 
-			if len(preCommitPhase[message['digest']].keys()) >= n - f:
-				preCommitPhase = {}
-				self.cPhase = 'propose'
-				self.round += 1
-				return True 
+		if message['value'] not in self.preCommitPhase['rmsg'].keys():
+			self.preCommitPhase['rmsg'][message['value']] = []
+		self.preCommitPhase['rmsg'][message['value']].append(message['sender'])
+
+		if len(self.preCommitPhase['rmsg'][message['value']]) >= n - f:
+			self.cPhase = 'precommit'
+			top_ele = self.heappop(self.heap)
+			# its a min heap so , 
+			top_ele[0]-=1
+			self.heap.heappush(self.heap , top_ele)
+			self.preCommitPhase = {'recvIds':[],'rmsg':{}}
+			return True 
 		else:
 			# duplicate message ignore this bitch 
 			return False
 
 	def start(self):
+		self.delay = 100
 		while True:
 			'''
 				Step 1: Recv msg 
@@ -74,6 +81,9 @@ class pBFT:
 			'''
 			# can check who is leader here...for view change and shit 
 
+			self.delay-=1
+			if(self.delay < 0):
+				break
 			rmsg = self.networkingObject.recvMessage()
 			rv = False
 			if rmsg == None:
@@ -89,5 +99,12 @@ class pBFT:
 				# signature shit here 
 				self.boradCast(rmsg)
 			elif rmsg['type'] == 'precommit' and self.preCommitPhase(rmsg):
-				# add to file or something 
+				# add to file or something
+				a=2
+
+			# else:
+			#     top_ele = self.heappop(self.heap)
+			#     # its a min heap so , 
+			#     top_ele[0]+=1
+			#     self.heap.heappush(self.heap , top_ele)
 
