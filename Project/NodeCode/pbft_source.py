8f4af2991e1f28e9063aa51ad9c408f3d8d9137e
varul <varul.srivastava@research.iiit.ac.in>
2023-04-22 02:41:50 +0530

    added code

--- Project/NodeCode/pbft_source.py
+++ Project/NodeCode/pbft_source.py
@@ -1,22 +1,28 @@
 import heapq
 class pBFT:
-	def __init__(self,identity:str,nodeList:list,networkObject):
+	def __init__(self,identity:str,nodeList:list,networkObject,maxDelay:int):
 		self.identity = identity
 		self.nodeList = nodeList
 		self.networkingObject = networkObject
 		self.round = 0
+		self.viewChangeType1 = []
 		self.currentMessage = {}
 		self.votingPhase = {}
 		self.preCommitPhase = {}
 		self.f = len(self.nodeList)//3
 		self.n = len(self.nodeList)
 		self.cPhase = 'propose'
-		self.delay = 100
+		self.delay = maxDelay
+		self.maxDelay = maxDelay
 		self.heap = []
 		for node in nodeList:
-			self.heap.heappush(self.heap , (0,node))
+			heapq.heappush(self.heap , [0,node])
+
+	def resetClock(self):
+		self.delay = self.maxDelay
+
 	def boradCast(self,message:dict):
-		for node in nodeList:
+		for node in self.nodeList:
 			self.networkingObject.sendMessage(node,message)
 
 	def votingPhase(self,message):
@@ -31,6 +37,7 @@ class pBFT:
 		self.votingPhase['rmsg'][message['value']].append(message['sender'])
 		if len(self.votingPhase['rmsg'][message['value']]) >= n - f:
 			self.cPhase = 'precommit'
+			self.resetClock()
 			self.votingPhase = {'recvIds':[],'rmsg':{}}
 			return True 
 		else:
@@ -42,8 +49,21 @@ class pBFT:
 			return False
 		else:
 			self.message = message
+			self.resetClock()
 			self.cPhase = 'voting'
 			return True
+
+	def changeLeader(self,score:int):
+		top_ele = heapq.heappop(self.heap)
+		# its a min heap so , 
+		top_ele[0] -= score
+		heapq.heappush(self.heap , top_ele)
+
+		self.resetClock()
+		self.preCommitPhase = {'recvIds':[],'rmsg':{}}
+		self.votingPhase = {'recvIds':[],'rmsg':{}}
+		self.message = {}
+		self.cPhase = 'propose'
 
 	def preCommitPhase(self,message):
 		if self.cPhase != 'precommit':
@@ -56,20 +76,47 @@ class pBFT:
 			self.preCommitPhase['rmsg'][message['value']] = []
 		self.preCommitPhase['rmsg'][message['value']].append(message['sender'])
 
-		if len(self.preCommitPhase['rmsg'][message['value']]) >= n - f:
+		if len(self.preCommitPhase['rmsg'][message['value']]) >= self.n - self.f:
 			self.cPhase = 'precommit'
-			top_ele = self.heappop(self.heap)
-			# its a min heap so , 
-			top_ele[0]-=1
-			self.heap.heappush(self.heap , top_ele)
+			self.resetClock()
+			self.changeLeader(1)
 			self.preCommitPhase = {'recvIds':[],'rmsg':{}}
 			return True 
+		elif len(self.preCommitPhase['rmsg'][message['value']]) >= self.f + 1 and self.message['value'] != message['value']:
+			self.viewChange(json.dumps({'single':self.message,'multiple':self.preCommitPhase['rmsg'][message['value']]}))
 		else:
 			# duplicate message ignore this bitch 
 			return False
 
+	def viewChange(self,proof=None):
+		if proof == None:
+			self.boradCast({'type':"view-change",'round':self.round,"identity":self.identity,'proof':"None"})
+		elif type(proof) == str:
+			self.boradCast({'type':"view-change",'round':self.round,"identity":self.identity,'proof':proof})
+
+	def viewChangeRecv(self,message):
+		if message['proof'] == "None" and message['identity'] not in self.viewChangeType1:
+			self.viewChangeType1.append(self.identity)		# can append signed message digest instead
+			if len(self.viewChangeType1) >= self.f + 1:
+				self.changeLeader(-1)
+			# commit to view change if len(self.viewChangeType1)
+		else:
+			proof_valid = True
+			# check if proof is correct 
+			if proof_valid:
+				self.changeLeader(-1)
+			pass
+
+	def addToFile(self,message:dict):
+		cleg = {}
+		with open('./'+self.identity+'_ledger.json','r') as f:
+			cleg = json.load(f)
+		
+		with open('./'+self.identity+'_ledger.json','w') as f:
+			json.dumps(cleg['ledger'].append(message))
+
+
 	def start(self):
-		self.delay = 100
 		while True:
 			'''
 				Step 1: Recv msg 
@@ -82,29 +129,36 @@ class pBFT:
 			# can check who is leader here...for view change and shit 
 
 			self.delay-=1
-			if(self.delay < 0):
-				break
+
+			if self.delay <= 0:
+				self.viewChange()
+
 			rmsg = self.networkingObject.recvMessage()
 			rv = False
+
 			if rmsg == None:
 				continue
-			if rmsg['type'] == 'propose' and self.proposalPhase(rmsg):
+
+			elif rmsg['round'] != self.round:
+				# ideally, save this message (if from future round) to be processed later, but for now ignore the message 
+				pass
+
+			elif rmsg['type'] == 'view-change':
+				self.viewChangeRecv(rmsg)
+
+			elif rmsg['type'] == 'propose' and self.proposalPhase(rmsg):
 				rmsg['type'] = 'vote'
 				rmsg['sender'] = self.identity
 				# signature shit here 
 				self.boradCast(rmsg)
+
 			elif rmsg['type'] == 'vote' and self.votingPhase(rmsg):
 				rmsg['type'] = 'precommit'
 				rmsg['sender'] = self.identity
 				# signature shit here 
 				self.boradCast(rmsg)
+
 			elif rmsg['type'] == 'precommit' and self.preCommitPhase(rmsg):
 				# add to file or something
-				a=2
-
-			# else:
-			#     top_ele = self.heappop(self.heap)
-			#     # its a min heap so , 
-			#     top_ele[0]+=1
-			#     self.heap.heappush(self.heap , top_ele)
+				self.addToFile(self.message)
 
---
+++ Project/NodeCode/pbft_test.py
@@ -0,0 +1,39 @@
+from pbft_source import pBFT
+from local_networking import Network
+from network_simulator import NetworkSimulator
+import sys
+import os 
+import json 
+import random
+import time 
+from threading import Thread
+from meta import NL,DELAY
+
+def makeFile(fileName,content):
+	with open(fileName,'w') as fp:
+		json.dump(content,fp)
+
+for fn in NL:
+	makeFile(fn+'_pre_messages.json',{"messages":[]})
+	makeFile(fn+'_messages.json',{"messages":[]})
+	makeFile(fn+'_lock.json',{"status":0})
+	makeFile(fn+'_ledger.json',{"ledger":[]})
+	
+# time for simulator to start 
+time.sleep(2)
+# for fn in NL:
+# 	add_elt(fn+'.json',{"hello":1,"world":-1})
+
+def startNode(node):
+	node.start()
+
+
+nodeList = []
+for fn in NL:
+	nodeList.append(pBFT(fn,NL,Network(fn,'./'),DELAY))
+
+for fn in nodeList:
+	thread = Thread(target=startNode,args=(fn,))
+	thread.start()
+	thread.join()
+
